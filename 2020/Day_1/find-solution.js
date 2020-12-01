1bc99e10a5f80411cb07c9df79be5073ccb1e6df
Daniel Klier <daniel.klier@mimacom.com>
2020-12-01 19:56:35 +0100

    Count steps

--- 2020/Day_1/find-solution.js
+++ 2020/Day_1/find-solution.js
@@ -5,13 +5,16 @@
  */
 function findSolution(input) {
   const targetSum = 2020;
+  let steps = 0;
 
   for (let i = 0; i<input.length;i++) {
     const a = input[i];
     for (let j = 0; j < input.length; j++) {
       const b = input[j];
       const sum = a + b;
+      ++steps;
       if (sum === targetSum) {
+        console.log(`Took ${steps} to find the solution`);
         return a * b;
       }
     }
