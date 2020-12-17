f4947c4a51953a5d6835cbf924c289d6ffebe336
Daniel Klier <daniel.klier@mimacom.com>
2020-12-17 20:39:09 +0100

    Day 17 part 2 - quick & dirty copy paste

--- 2020/Day_17/index.ts
+++ 2020/Day_17/index.ts
@@ -3,6 +3,7 @@ import { lines } from "../utils/input.ts";
 import { assertEquals } from "../utils/test.ts";
 
 type Coords = [number, number, number];
+type Coords4 = [number, number, number, number];
 const CYCLES = 6;
 
 function createCoords(length: number): string[][][] {
@@ -65,21 +66,82 @@ function simulateOne(coords: string[][][]): string[][][] {
   return newCoords;
 }
 
-function draw(coords: string[][][]) {
-  coords.forEach((plane, i) => {
-    console.log("z =", i - Math.floor(coords.length / 2));
-    plane.forEach((row) => console.log(row.join("")));
-  });
+function createCoords4(length: number): string[][][][] {
+  const coords = Array.from({ length }, () =>
+    Array.from({ length }, () =>
+      Array.from({ length }, () => Array.from({ length }, () => "."))
+    )
+  );
+  return coords;
+}
+
+function parseInput4(input: string[]): string[][][][] {
+  const offset = CYCLES + 1;
+  const maxN = input.length + offset * 2;
+  const coords = createCoords4(maxN);
+  for (let y = 0; y < input.length; y++) {
+    const line = input[y];
+    for (let x = 0; x < input.length; x++) {
+      const [i, j, k, l] = [x + offset, y + offset, offset, offset];
+      coords[l][k][j][i] = line[x];
+    }
+  }
+  return coords;
+}
+
+function neighborCoords4(
+  i: number,
+  j: number,
+  k: number,
+  l: number
+): Coords4[] {
+  const coords: Coords4[] = [];
+  for (let a = -1; a <= 1; a++) {
+    for (let b = -1; b <= 1; b++) {
+      for (let c = -1; c <= 1; c++) {
+        for (let d = -1; d <= 1; d++) {
+          if (a === 0 && b === 0 && c === 0 && d === 0) {
+            continue;
+          }
+          coords.push([i + a, j + b, k + c, l + d]);
+        }
+      }
+    }
+  }
+  return coords;
+}
+
+function simulateOne4(coords: string[][][][]): string[][][][] {
+  const N = coords.length;
+  const newCoords = createCoords4(N);
+  for (let i = 1; i < N - 1; i++) {
+    for (let j = 1; j < N - 1; j++) {
+      for (let k = 1; k < N - 1; k++) {
+        for (let l = 1; l < N - 1; l++) {
+          const currentCube = coords[i][j][k][l];
+
+          const numNeighbors = neighborCoords4(i, j, k, l).filter(
+            ([a, b, c, d]) => coords[a][b][c][d] === "#"
+          ).length;
+          if (currentCube === "#" && numNeighbors !== 2 && numNeighbors !== 3) {
+            newCoords[i][j][k][l] = ".";
+          } else if (currentCube === "." && numNeighbors === 3) {
+            newCoords[i][j][k][l] = "#";
+          } else {
+            newCoords[i][j][k][l] = currentCube;
+          }
+        }
+      }
+    }
+  }
+  return newCoords;
 }
 
 function part1(input: string[]): number {
-  console.log(input);
   let coords = parseInput(input);
-  //draw(coords);
   for (let t = 0; t < CYCLES; t++) {
     coords = simulateOne(coords);
   }
-  //draw(coords);
   return coords.reduce(
     (sum, arr) =>
       sum +
@@ -92,8 +154,26 @@ function part1(input: string[]): number {
   );
 }
 
-function part2(input: unknown[]): number {
-  return 0;
+function part2(input: string[]): number {
+  let coords = parseInput4(input);
+  for (let t = 0; t < CYCLES; t++) {
+    coords = simulateOne4(coords);
+  }
+  return coords.reduce(
+    (sum0, arr0) =>
+      sum0 +
+      arr0.reduce(
+        (sum1, arr1) =>
+          sum1 +
+          arr1.reduce(
+            (sum2, arr2) =>
+              sum2 + arr2.reduce((sum3, v) => (v === "#" ? sum3 + 1 : sum3), 0),
+            0
+          ),
+        0
+      ),
+    0
+  );
 }
 
 async function day17(input: string): Promise<void> {
